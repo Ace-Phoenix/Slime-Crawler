895afcba17f70218d02b8feb968ace776dd0059c
Euan Barclay <Silverswimmer63@gmail.com>
2021-05-13 11:38:05 -0400

    added the beginings of armors

--- root/items/armorList.js
+++ root/items/armorList.js
@@ -1,15 +1,36 @@
 // Coder : E.B.
-// Ideas : Euan ,Owen, Kennedy 
+// Ideas : Euan ,Owen, Kennedy
 //the variable name tells what slot it goes in it does not mean it has to be a helmet
 //or have the type helmet it should just make sence in that slot such as for a helmet slot
 //you could use hood or crown
 var helmetArmors = [
+  {
+    name: ,
+    type: ,
+    value: ,
+    desc: ,
+    damRes: ,
+  }
 ];
 //same thing with the chestArmors it does not have to be a chest plate
 //it could be robes or anything else you can think of
 var chestArmors = [
+  {
+    name: ,
+    type: ,
+    value: ,
+    desc: ,
+    damRes: ,
+  }
 ];
 
-//same thing with the boots 
+//same thing with the boots
 var bootArmors = [
+  {
+    name: ,
+    type: ,
+    value: ,
+    desc: ,
+    damRes: ,
+  }
 ];
