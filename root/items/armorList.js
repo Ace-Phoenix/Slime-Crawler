a49fdb571b9f809d7104e485a9cfd10b1d413294
Euan Barclay <Silverswimmer63@gmail.com>
2021-05-13 11:55:03 -0400

    added some basic armor

--- root/items/armorList.js
+++ root/items/armorList.js
@@ -1,36 +1,64 @@
 // Coder : E.B.
 // Ideas : Euan ,Owen, Kennedy
+//max level 10
 //the variable name tells what slot it goes in it does not mean it has to be a helmet
 //or have the type helmet it should just make sence in that slot such as for a helmet slot
 //you could use hood or crown
-var helmetArmors = [
+const helmetArmors = [
   {
-    name: ,
-    type: ,
-    value: ,
-    desc: ,
-    damRes: ,
+    name: "Basic Helm",
+    type: helmet,//other type is hoood
+    level: 1,
+    desc: "A simple leather helm that cushions blows",
+    damRes: 1,
+    magic: "none"
+  },
+  {
+    name: "Basic Hood",
+    type: hood,
+    level: 1,
+    desc: "A simple hood that increases your Mana",
+    damRes: 0,
+    magic: "moreMana"
   }
 ];
 //same thing with the chestArmors it does not have to be a chest plate
 //it could be robes or anything else you can think of
-var chestArmors = [
+const chestArmors = [
   {
-    name: ,
-    type: ,
-    value: ,
-    desc: ,
-    damRes: ,
+    name: "Basic chestpiece",
+    type: chestplate,
+    level: 1,
+    desc: "A simple leather chestpiece that cushions blows",
+    damRes: 1,
+    magic: "none"
+  },
+  {
+    name: "Basic Robe",
+    type: robe,
+    level: 1,
+    desc: "A simple robe that increases your mana",
+    damRes: 0,
+    magic: "moreMana"
   }
 ];
 
 //same thing with the boots
-var bootArmors = [
+const bootArmors = [
   {
-    name: ,
-    type: ,
-    value: ,
-    desc: ,
-    damRes: ,
+    name: "Basic Shoes",
+    type: boot,
+    level: 1,
+    desc: "Simple boots that cushions blows",
+    damRes: 1,
+    magic: "none",
+  },
+  {
+    name: "Magic Shoes",
+    type: shoe,
+    level: 1,
+    desc: "Magic Shoes for more magicing",
+    damRes: 0,
+    magic: "moreMana"
   }
 ];
